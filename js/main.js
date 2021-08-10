2a5c7a17a062da7f7e1dd33c8f580ee177cf7922
emreusun <emre95neo@gmail.com>
2021-08-09 22:17:36 -0400

    video bugs fixed

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ The Lannisters rule over the Westerlands. Their seat is Casterly Rock, a massive
 
       ["Tully", `House Tully of Riverrun is an exiled Great House of Westeros. Its most senior member carried the title of Lord of Riverrun and Lord Paramount of the Trident, until the Red Wedding. The current head is Lord Edmure Tully, son of the late Hoster Tully. The Tully sigil is a silver trout on a red and blue background. Their house words are "Family, Duty, Honor."
 `],
-
       ["Greyjoy", `House Greyjoy of Pyke is one of the Great Houses of Westeros. It rules over the Iron Islands, a harsh and bleak collection of islands off the west coast of Westeros, from the castle at Pyke. The head of the house is the Lord Reaper of Pyke.
 
 House Greyjoy's sigil is traditionally a golden kraken on a black field. Their house words are "We Do Not Sow," although the phrase "What Is Dead May Never Die" is also closely associated with House Greyjoy and their bannermen, as they are associated with the faith of the Drowned God. `],
